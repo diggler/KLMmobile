af5059cfb6813a23328137435308ecc2b845ac1d
Dan Diggler <daniel.proczko@gmail.com>
2012-07-03 11:22:43 -0400

    MySQL testing
    First stage is figuring out the MySQL query to obtain all events from
    Wordpress database.

---
+++ connection.php
@@ -0,0 +1,5 @@
+<?php
+$connect = mysql_connect("apt1201803051010.db.2671188.hostedresource.com","apt1201803051010","UgLojF4G")or die(mysql_error());
+mysql_select_db("apt1201803051010",$connect)or die(mysql_error());
+?> 
+	
