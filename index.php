b67f5c4024c13c34a2cd56ff14bb3f6f91082725
Dan Diggler <daniel.proczko@gmail.com>
2012-07-03 13:32:00 -0400

    SIngle SQl Query updates
    Improved SQL query. Condensed SQL query into 1 query, but is now
    creating multiple rows of same content.

--- index.php
+++ index.php
@@ -36,35 +36,42 @@
     <?php //database select
 		$post = "SELECT * FROM wp_posts 
 		INNER JOIN wp_postmeta ON wp_posts.ID=wp_postmeta.post_id 
+		INNER JOIN wp_term_relationships ON wp_posts.ID=wp_term_relationships.object_id
+		INNER JOIN wp_term_taxonomy ON wp_term_taxonomy.term_taxonomy_id=wp_term_relationships.term_taxonomy_id
+		INNER JOIN wp_terms ON wp_terms.term_id=wp_term_taxonomy.term_id
 		WHERE post_type='event' AND post_status='publish' AND meta_key = 'date' AND STR_TO_DATE(meta_value,'%m/%d/%Y') >= STR_TO_DATE('".date('Y-m-d 00:00:00')."', '%Y-%m-%d %H:%i:%s') ORDER BY STR_TO_DATE(meta_value,'%m/%d/%Y') ASC"; 
 		$posts = mysql_query($post, $connect);
 		echo date('Y-m-d H:i:s')."<br>";
 		echo  mysql_num_rows ($posts)."<br>";
 		while ($events = mysql_fetch_array($posts, MYSQL_ASSOC)) {
 			
-			$start_query = "SELECT meta_value FROM wp_postmeta WHERE post_id='".$events['ID']."' AND meta_key='start_time'";
-			$start_time = mysql_query($start_query, $connect);
-			$genre_query = "
-			SELECT * FROM wp_terms  
-			INNER JOIN wp_term_taxonomy ON wp_term_taxonomy.term_id=wp_terms.term_id
-			INNER JOIN wp_term_relationships ON wp_term_relationships.term_taxonomy_id=wp_term_taxonomy.term_taxonomy_id
-			WHERE object_id='".$events['ID']."' 
-			AND taxonomy='genres'";
-			$genres = mysql_query($genre_query, $connect) or die("No Results");
-			//$venue_query = ;
-			//$band_query = ;
+			//$start_query = "SELECT meta_value FROM wp_postmeta WHERE post_id='".$events['ID']."' AND meta_key='start_time'";
+//			$start_time = mysql_query($start_query, $connect);
+//			$genre_query = "
+//			SELECT * FROM wp_terms  
+//			INNER JOIN wp_term_taxonomy ON wp_term_taxonomy.term_id=wp_terms.term_id
+//			INNER JOIN wp_term_relationships ON wp_term_relationships.term_taxonomy_id=wp_term_taxonomy.term_taxonomy_id
+//			WHERE object_id='".$events['ID']."' 
+//			AND taxonomy='genres'";
+//			$genres = mysql_query($genre_query, $connect) or die("No Results");
+//			//$venue_query = ;
+//			//$band_query = ;
 			echo $events['ID'];
 			print_r($events['post_title']); echo "<br>";
 			print_r($events['meta_value']); echo "<br>";
-			//start time query loop
-			while ($start = mysql_fetch_array($start_time, MYSQL_ASSOC)){
-				print_r($start['meta_value']); echo "<br>";
-			}
-			//genre query loop
-			while ($genre = mysql_fetch_array($genres, MYSQL_ASSOC)){
-				print_r($genre['name']); echo "<br>";
-			}
-			echo "<br>";
+			print_r($events['name']); echo "<br>";
+			print_r($events['start_time']); echo "<br>";
+			print_r($events['start_time']); echo "<br>";
+			print_r($events['start_time']); echo "<br>";
+			////start time query loop
+//			while ($start = mysql_fetch_array($start_time, MYSQL_ASSOC)){
+//				print_r($start['meta_value']); echo "<br>";
+//			}
+//			//genre query loop
+//			while ($genre = mysql_fetch_array($genres, MYSQL_ASSOC)){
+//				print_r($genre['name']); echo "<br>";
+//			}
+//			echo "<br>";
 		}
 		//$results = mysql_fetch_assoc($posts);
 		//print_r($results);
